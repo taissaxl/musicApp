1082080e1a47f1bab176a640a08ad047aa7096fd
taissaxl <taissa.leal@estudante.ufjf.br>
2023-08-02 15:25:00 -0300

    Add fields to Audio.java

--- src/com/music/musicapp/models/Audio.java
+++ src/com/music/musicapp/models/Audio.java
@@ -1,4 +1,10 @@
 package com.music.musicapp.models;
 
 public class Audio {
+    private String title;
+    private int duration;
+    private int totalListeners;
+    private int likes;
+    private double rating;
+
 }
