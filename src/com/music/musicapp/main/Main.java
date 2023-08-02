af5efb39a1004d5cb99fb10547bc9d966c2928d7
taissaxl <taissa.leal@estudante.ufjf.br>
2023-08-02 20:41:08 -0300

    Refactor static tests

--- src/com/music/musicapp/main/Main.java
+++ src/com/music/musicapp/main/Main.java
@@ -2,14 +2,18 @@ package com.music.musicapp.main;
 
 import com.music.musicapp.models.Music;
 import com.music.musicapp.models.Podcast;
+import com.music.musicapp.models.Ratings;
 
 public class Main {
     public static void main(String[] args) {
         Music music = new Music();
+        Podcast podcast = new Podcast();
+        Ratings ratings = new Ratings();
+
         music.setTitle("NASA");
         music.setArtist("Ariana Grande");
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 254987234; i++) {
             music.playAudio();
         }
 
@@ -19,8 +23,8 @@ public class Main {
 
         System.out.println("Total listeners to 'NASA' by Ariana Grande: " + music.getTotalListeners());
         System.out.println("Likes: " + music.getLikes());
+        ratings.include(music);
 
-        Podcast podcast = new Podcast();
         podcast.setTitle("Podcast but Outside");
         podcast.setHost("Andrew and Cole");
 
@@ -34,5 +38,6 @@ public class Main {
 
         System.out.println("Total listeners to Podcast But Outside: " + podcast.getTotalListeners());
         System.out.println("Likes: " + podcast.getLikes());
+        ratings.include(podcast);
     }
 }
