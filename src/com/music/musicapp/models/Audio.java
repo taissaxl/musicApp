de96b965304cc55da9b90de954cb3056c85cb981
taissaxl <taissa.leal@estudante.ufjf.br>
2023-08-02 20:01:07 -0300

    Refactor methods

--- src/com/music/musicapp/models/Audio.java
+++ src/com/music/musicapp/models/Audio.java
@@ -2,9 +2,42 @@ package com.music.musicapp.models;
 
 public class Audio {
     private String title;
-    private int duration;
+    private double duration;
     private int totalListeners;
     private int likes;
     private double rating;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public int getTotalListeners() {
+        return totalListeners;
+    }
+
+    public int getLikes() {
+        return likes++;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void likes(){
+        this.likes++;
+    }
+
+    public void playAudio(Audio audio) {
+        System.out.println("Now playing");
+        this.totalListeners++;
+    }
 
 }
