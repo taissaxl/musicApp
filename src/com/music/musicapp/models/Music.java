06142a8744dde41d2da231eeb5500dcd5dfdce15
taissaxl <taissa.leal@estudante.ufjf.br>
2023-08-02 15:44:27 -0300

    Add fields and methods to Music.java

--- src/com/music/musicapp/models/Music.java
+++ src/com/music/musicapp/models/Music.java
@@ -1,4 +1,22 @@
 package com.music.musicapp.models;
 
-public class Music {
+public class Music extends Audio {
+    private String genre;
+    private String artist;
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
 }
