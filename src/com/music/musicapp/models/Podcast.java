724df7df364e7e079ddf846d808a1b39e911fba1
taissaxl <taissa.leal@estudante.ufjf.br>
2023-08-02 15:44:43 -0300

    Add fields and methods to Podcast.java

--- src/com/music/musicapp/models/Podcast.java
+++ src/com/music/musicapp/models/Podcast.java
@@ -1,4 +1,31 @@
 package com.music.musicapp.models;
 
-public class Podcast {
+public class Podcast extends Audio {
+    private String host;
+    private String guests;
+    private String theme;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getGuests() {
+        return guests;
+    }
+
+    public void setGuests(String guests) {
+        this.guests = guests;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
 }
