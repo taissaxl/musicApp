cf3b9c1a004eb263cf9dc50664e19e2502afa45e
taissaxl <taissa.leal@estudante.ufjf.br>
2023-08-02 20:06:32 -0300

    Move main to package

---
+++ src/com/music/musicapp/main/Main.java
@@ -0,0 +1,10 @@
+package com.music.musicapp.main;
+
+import com.music.musicapp.models.Music;
+
+public class Main {
+    public static void main(String[] args) {
+        Music music = new Music();
+
+    }
+}
